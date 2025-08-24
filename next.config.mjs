2190073a2fa16aa1aa1c94caea2321103c9455a2
E. Guernina <elhem358@proton.me>
2025-08-24 12:08:54 +0200

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  distDir: 'dist',
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+}
+
+export default nextConfig
